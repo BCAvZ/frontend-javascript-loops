073c2aaf4d9c25c5c1be0c3aee29117aad83ce80
BCAvZ <Bartvanzanen@hotmail.com>
2022-06-19 19:15:56 +0200

    huiswerk af

--- loops.js
+++ loops.js
@@ -60,11 +60,11 @@ for(let i = 0; i < 5; i++){
 // ==========================================
 
 for (i = 0; i <16; i++) {
-    if (i % 2) {
-        console.log(i + ' is oneven')
+    if (i % 2 === 0) {
+        console.log(i + ' is even')
     }
     else {
-        console.log(i + ' is even')
+        console.log(i + ' is oneven')
     }
 }
 
